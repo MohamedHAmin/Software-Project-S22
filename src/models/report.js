eac44cc4dbc83a1d141918e82b2357f91f073170
Abdelkhalek7 <100151191+Abdelkhalek7@users.noreply.github.com>
2022-03-21 19:44:52 +0200

    1

--- src/models/report.js
+++ src/models/report.js
@@ -7,6 +7,7 @@ const reportSchema = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         required:true,
         ref:'User'
+        
     },
     msg:{
         type:String  
