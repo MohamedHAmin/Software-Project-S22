32d2e4741ecda514d824106b73d72e32a1cc4667
Abdelkhalek7 <100151191+Abdelkhalek7@users.noreply.github.com>
2022-03-30 21:17:11 +0200

    follow added

--- src/app.js
+++ src/app.js
@@ -6,12 +6,14 @@ require('./db/mongoose')
 const adminRouter =require('./routers/admin')
 const userAuthRouter =require('./routers/userauth')
 const tweetRouter =require('./routers/tweet')
+const followRouter =require('./routers/followroute')
 
 const app=express()
 const port=process.env.PORT
 app.use(express.json())
 app.use(userAuthRouter)
 app.use(tweetRouter)
+app.use(followRouter)
 app.use('/admin',adminRouter)
 
 module.exports =app
--- src/routers/followroute.js
+++ src/routers/followroute.js
@@ -2,57 +2,36 @@ const express = require("express");
 const User = require("../models/User");
 const auth = require("../middleware/auth");
 const router = new express.Router();
-  /////////////~~~~~~~~~~~~~~~~login~~~~~~~~~`////
-  router.post("/login", async (req, res) => {
-    try {
-      const user = await User.findbycredentials(
-        req.body.email,
-        req.body.password
-      );
-      const token = await user.generateAuthToken();
-      res.send({ user, token });
-    } catch (e) {
-      res.status(400).send("error");
-    }
-  });
-  /////~~~~~~~~~~~~~~logout~~~~~~~~~~~~~~////
-  router.post("/logout", auth, async (req, res) => {
-    try {
-      req.user.tokens = req.user.tokens.filter((token) => {
-        return token.token != req.token;
-      });
-      await req.user.save();
-      res.send("user");
-    } catch (e) {
-      res.status(400).send("error");
-    }
-  });
+
+  
   ////~~~~~~~~~~~~~~~follow~~~~~~~~~~~~~~/////
-  router.post("/follow/:id",auth, async (req, res) => {
+  router.post("/user/:userId/follow/:id",auth("user"), async (req, res) => {
     const user=await User.findById(req.params.id)
     try {
+      if(!user){
+        throw new Error("no user found")
+      }
       user.followercount++
       await user.save()
-    
       req.user.following=req.user.following.concat({followingId:user._id})
       req.user.followedcount++
       await req.user.save()
-  
       const n=req.user
-      res.send({ user,n});
+      res.send({ user});
     } catch (e) {
-      res.status(400).send("error"+e);
+      res.status(400).send({error:e});
     }
   });
   ///////////***unfollow */
-  router.post("/unfollow/:id", auth, async (req, res) => {
+  router.post("/user/:userId/unfollow/:id", auth("user"), async (req, res) => {
     const user=await User.findById(req.params.id)
     try {
-  
+        if(!user){
+          throw new Error("no user found")
+        }
       req.user.following = req.user.following.filter((follow) => {
-        return follow.followingID != req.userID;
+        return follow.followingId != user._id;
       });
-      
       await req.user.save();
       res.send("user");
     } catch (e) {
@@ -61,39 +40,33 @@ const router = new express.Router();
   });
   
   
-  router.get("/following/:id",auth, async (req, res) => {
+  router.get("/user/:id/following",auth("user"), async (req, res) => {
     const user=await User.findById(req.params.id)
-  
     try {
       await user.populate({
         path: "following.followingId",
+        select: 'userId screenName'
       });
-      console.log(user.following[0].followingId);
-      user.following.map(follow=>{
-        console.log('1');
-         follow.followingId.password=''
-        console.log( follow.followingId.password)
-  
-      })
-      // if(user.following.userId.password){
-      //    user.following.userId.password=''
-      //    user.following.userId.tokens=''}
-       
-      res.send(user);
+     
+      res.send(user.following);
     } catch (e) {
       res.status(400).send("error"+e);
     }
   });
-  router.get("/follower/:id",auth, async (req, res) => {
+  router.get("/user/:id/follower",auth("user"), async (req, res) => {
     const user=await User.findOne({ _id:req.params.id})
     try {
       await user.populate( {
         path: "follower",
+        select: '_id screenName tag followercount followingcount',
+        options:{
+          limit: parseInt(req.query.limit),
+
+          skip: parseInt(req.query.skip),
+        }
       });
       
-      console.log(user);
-      
-      res.send({ user});
+      res.send(user.follower);
     } catch (e) {
       res.status(400).send("error"+e);
     }
@@ -101,5 +74,5 @@ const router = new express.Router();
   
   
   
-  module.exports = router
+  module.exports = router 
   
