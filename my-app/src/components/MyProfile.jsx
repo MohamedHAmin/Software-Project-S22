fb5bf8c971302ada5053c506cec16132980edfb2
AmroY1209 <amryasser1209@gmail.com>
2022-04-01 09:31:11 +0200

    img placeholder
    doggo img

--- my-app/src/components/MyProfile.jsx
+++ my-app/src/components/MyProfile.jsx
@@ -10,7 +10,7 @@ function MyProfile() {
     <div className="myProfile">
     <ProfileName/>
     <div className="profileSetup">
-    <Avatar className="profileImage" alt="Ahmed Emad" src="https://scontent.fcai19-4.fna.fbcdn.net/v/t1.6435-9/84551579_2716162291800662_5091543180898205696_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHrnZB4gKSGUwMf0rXbcf3jbdijfPnXOs1t2KN8-dc6zSzNPTvMnfxioyME6NpeGoUVWXaEN64OW5sKZg21tByM&_nc_ohc=8fKqhHu9fyIAX-wr-aF&_nc_ht=scontent.fcai19-4.fna&oh=00_AT8cG1UGDie_M14Z4VV-phl6Q1qb6PMK--0UpJ7d9Zq4MQ&oe=62656AAB" />
+    <Avatar className="profileImage" alt="Ahmed Emad" src="https://i.picsum.photos/id/1025/4951/3301.jpg?hmac=_aGh5AtoOChip_iaMo8ZvvytfEojcgqbCH7dzaz-H8Y" />
     <Button className="setupProfileButton" variant="outlined" fullWidth>Set Up Profile
     </Button>
     </div>
