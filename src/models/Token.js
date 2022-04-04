2152fab4b24c214d204b3f9011e4175a7c9d4c2a
Oz174 <83515041+Oz174@users.noreply.github.com>
2022-04-04 16:33:13 +0200

    Renamed token to Token

---
+++ src/models/Token.js
@@ -0,0 +1,22 @@
+const mongoose =require('mongoose')
+
+const tokenSchema = new mongoose.Schema({
+
+    token:{
+        type:String,
+        required:true
+    },
+    ownerId:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true
+    }
+},
+{
+    timestamps:true,
+    toJSON: {virtuals: true}
+        
+ });
+      
+      const Token = mongoose.model('Token', tokenSchema);
+      
+      module.exports = Token
