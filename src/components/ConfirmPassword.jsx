192fc6a218cac4c3a30fc1585ed69e2e1fc8fef3
karimyasser107 <75176822+karimyasser107@users.noreply.github.com>
2022-04-12 14:08:19 +0200

    confirm password updated 30%

--- src/components/ConfirmPassword.jsx
+++ src/components/ConfirmPassword.jsx
@@ -1,21 +1,18 @@
 import React, { useState } from 'react';
-import './Styles/SubMenu.css'
-import PersonIcon from '@mui/icons-material/Person';
-import KeyIcon from '@mui/icons-material/Key';
-import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import FilledInput from '@mui/material/FilledInput';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Button from '@mui/material/Button';
 import "./Styles/SettingsMenu.css"
 function ConfirmPassword(props) {
+  //variable to check if password id entered correctly
+  const[ispasswordCorrect,setPasswordCorrect]=useState(false);
+  const[isClickedConfirm,setIsClickedConfirm]=useState(false);
+  const [confirmPassValueError, setValueConfirmPassError] = useState(false);
     const [values, setValues] = React.useState({
         amount: '',
         password: '',
@@ -37,17 +34,36 @@ function ConfirmPassword(props) {
       const handleMouseDownPassword = (event) => {
         event.preventDefault();
       };
-    
+      //function to check on entered password
+      const handleClickConfirmPass= () => {
+        setValueConfirmPassError(false);
+        if (values.password.length===0) {
+        }
+        if(values.password.length>0){
+            setIsClickedConfirm(true);
+          
+          if(values.password==="karim"){
+          setIsClickedConfirm(true);
+          setPasswordCorrect(true);
+          props.onConfirmPassChange()
+          }
+          else{
+            setValueConfirmPassError(true);
+          }
+        }
+      }
     
     return ( 
-        <div className="settingsMenu">
-        {/* <h1 className={props.isDarkMode? "settingsMenuHeaderLight":"settingsMenuHeaderDark" }>Account information</h1> */}
-        <h2 className={!props.darkMode? "settingsMenuHeaderLight":"settingsMenuHeaderDark" }>Confirm your password</h2>
+      <div >
+        <h2 style={{marginTop:30}} className={!props.darkMode? "settingsMenuHeaderLight":"settingsMenuHeaderDark" } >Confirm your password</h2>
         <p className={!props.darkMode? "settingsMenuParagraphLight":"settingsMenuParagraphDark" }>Please enter your password in order to get this.</p>
+        {/* recieve input password from usser */}
         
-        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+        <FormControl  style={{marginTop:30, marginLeft:20}} sx={{ m: 1, width: '25ch' }} variant="outlined">
           <InputLabel htmlFor="outlined-adornment-password" className={props.darkMode &&('forceChangePasswordMUIDarkMode')}>Password</InputLabel>
           <OutlinedInput
+          inputProps={{className:props.darkMode &&('forceChangePasswordMUIDarkMode') }}
+          helperText="The password you entered was incorrect."
             id="outlined-adornment-password"
             type={values.showPassword ? 'text' : 'password'}
             value={values.password}
@@ -65,13 +81,19 @@ function ConfirmPassword(props) {
               </InputAdornment>
             }
             label="Password"
+            error={confirmPassValueError}
+            //helperText="The password you entered was incorrect."
           />
-        </FormControl>
-       
-        
-        </div>
+          {/* wait for user to click on button confirm */}
+          <div style={{marginTop:15, marginLeft:163}}><Button onClick={handleClickConfirmPass} variant="contained">Confirm</Button></div>
+        </FormControl> 
+        {/* user clicks on button confirm but nothing entered */}
+        {/* {isClickedConfirm===true && values.password.length===0 && <h4 className={!props.darkMode? "settingsMenuParagraphLight":"settingsMenuParagraphDark" } style={{color:'#e53935'}}>Please enter your password!</h4>} */}
+        {/* user clicks on button confirm but password incorrect */}
+        {isClickedConfirm===true && ispasswordCorrect===false && (<h4 className={!props.darkMode? "settingsMenuParagraphLight":"settingsMenuParagraphDark" } style={{color:'#e53935'}}>The password you entered was incorrect.</h4>)}
+      </div>
 
-     );
+  );
 }
 
 export default ConfirmPassword;
