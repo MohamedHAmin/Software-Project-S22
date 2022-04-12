ec36db603104e583832b6f3d86f672c006b8c466
karimyasser107 <75176822+karimyasser107@users.noreply.github.com>
2022-04-12 14:10:42 +0200

    notification settings updated phase 30%

--- src/components/NotificationSettings.jsx
+++ src/components/NotificationSettings.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import SettingsMenuOptions from './SettingsMenuOptions';
-import './Styles/SubMenu.css';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import EmailIcon from '@mui/icons-material/Email';
 import "./Styles/SettingsMenu.css"
@@ -16,14 +15,12 @@ function NotificationSettings(props) {
         setEmailNotificationActive(true);        
     }
     return ( 
-        <div className="settingsMenu">
-        <h1 className={!props.isDarkMode? "settingsMenuHeaderLight":"settingsMenuHeaderDark" }>Notifications prefrences</h1>
-        <p className={!props.isDarkMode? "settingsMenuParagraphLight":"settingsMenuParagraphDark" }>Select your preferences by notification type.<a className='App-link' href="https://help.twitter.com/en/managing-your-account/notifications-on-mobile-devices">Learnmore</a></p>
-        <div onClick={clickedPushNotification} ><SettingsMenuOptions id="8" darkMode={props.isDarkMode} active={isClickedPushNotification} Icon={NotificationsIcon} text="Push notification"/></div>
-        <div onClick={clickedEmailNotification} ><SettingsMenuOptions id="9" darkMode={props.isDarkMode} Icon={EmailIcon} active={isClickedEmailNotification}  text="Email notification" /></div>
-        
+        <div className="settingsSubMenu">
+            <h1 className={!props.isDarkMode? "settingsMenuHeaderLight":"settingsMenuHeaderDark" }>Notifications prefrences</h1>
+            <p className={!props.isDarkMode? "settingsMenuParagraphLight":"settingsMenuParagraphDark" }>Select your preferences by notification type.<a className='App-link' href="https://help.twitter.com/en/managing-your-account/notifications-on-mobile-devices">Learnmore</a></p>
+            <div onClick={clickedPushNotification} ><SettingsMenuOptions id="8" darkMode={props.isDarkMode} active={isClickedPushNotification} Icon={NotificationsIcon} text="Push notification"/></div>
+            <div onClick={clickedEmailNotification} ><SettingsMenuOptions id="9" darkMode={props.isDarkMode} Icon={EmailIcon} active={isClickedEmailNotification}  text="Email notification" /></div>
         </div>
-
      );
 }
 
