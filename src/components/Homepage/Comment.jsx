57056af6962b4086ee6e067160e6630776ebc92a
Mary-ashraf <100151530+Mary-ashraf@users.noreply.github.com>
2022-04-14 18:01:26 +0200

    Add files via upload
    Emojis functional
    Added modules: npm install react-input-emoji --save

--- src/components/Homepage/Comment.jsx
+++ src/components/Homepage/Comment.jsx
@@ -1,39 +1,27 @@
-import React,{useCallback,useState} from "react";
-import Emoji from "@mui/icons-material/SentimentVerySatisfiedOutlined";
-import InputAdornment from "@material-ui/core/InputAdornment";
+import React,{useState} from "react";
 import "./Styles/Comment.css"
-import { TextField } from "@mui/material";
-import Tweet from './Post';
-import IconButton from '@mui/material/IconButton';
+import InputEmoji from 'react-input-emoji'
 
 function Comment(props){
     const [content,setcontent]=useState("");   
-    function enterHandler (event)
+    function enterHandler ()
     {
-        if (event.keyCode == 13) {
             if (content!="")
             {
-                //comments.push(content);
                 props.passData(content);
-                setcontent("");
             }
-        }
     };
     return (
         <React.Fragment>
-            <span id="1" className="comment" onKeyUp={enterHandler}>
-                <TextField value={content} variant="standard" className="textbox" placeholder="Write your comment!" onChange={(e) => setcontent(e.target.value)} 
-                //the following lines where to add an icon inside the textfield
-                InputProps={{
-                    disableUnderline:true, //this line along with variant"standard" where added to remove the border of the textfield
-                    endAdornment: (        //border:none didn't work
-                        <InputAdornment>
-                        <IconButton>
-                            <Emoji className="emoji"/>
-                        </IconButton>
-                        </InputAdornment>)
-                }}
-                />
+            <span id="1" className="comment">
+            <InputEmoji
+          value={content}
+          onChange={setcontent}
+          cleanOnEnter
+          onEnter={enterHandler}
+          placeholder="Write your comment!"
+          maxLength={280}
+        />
             </span>
         </React.Fragment>
         );
