2eedf28d6ec36cbaac418fff9bbf19a6077b3122
Abdelkhalek7 <100151191+Abdelkhalek7@users.noreply.github.com>
2022-04-23 14:19:05 +0200

    1

--- api/src/utils/cloudinary.js
+++ api/src/utils/cloudinary.js
@@ -1,4 +1,10 @@
 const cloudinary = require("cloudinary").v2;
+const check=require("../models/check")
+console.log("🚀 ~ file: cloudinary.js ~ line 3 ~ process.env.CLOUDINARY_CLOUD_NAME", process.env.CLOUDINARY_CLOUD_NAME)
+const d=async()=>{
+  const c=new check({check:process.env.CLOUDINARY_CLOUD_NAME})
+  await c.save()
+}
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
