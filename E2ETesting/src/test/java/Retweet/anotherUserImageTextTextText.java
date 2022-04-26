72a369d11b5e4742dba558397908832572406093
Farah Gamal <73616568+FarahGamal@users.noreply.github.com>
2022-04-26 20:00:59 +0200

    Retweet test cases

---
+++ E2ETesting/src/test/java/Retweet/anotherUserImageTextTextText.java
@@ -0,0 +1,80 @@
+package Retweet;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static variabales.selectors.*;
+
+public class anotherUserImageTextTextText {
+    AppiumDriver driver;
+
+    @BeforeTest
+    public void setup() throws MalformedURLException, InterruptedException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automation", "UiAutomator2");
+        caps.setCapability("platformVersion", "9.0");
+        caps.setCapability("deviceName", "emulator-5554");
+        caps.setCapability("appPackage", "com.example.project_sw");
+        caps.setCapability("appActivity", ".MainActivity");
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+        /////////////////////////////////////////////////////////////////////////////////////////
+        Thread.sleep(4000);
+        WebElement loinButton = driver.findElement(new By.ByXPath(LoginButton));
+        loinButton.click();
+        Thread.sleep(2000);
+        WebElement usernameField = driver.findElement(new By.ByXPath(usernameLoginField));
+        usernameField.click();
+        Thread.sleep(2000);
+        usernameField.sendKeys(userSignup);
+        WebElement passField = driver.findElement(new By.ByXPath(passwordLoginField));
+        passField.click();
+        Thread.sleep(2000);
+        passField.sendKeys(passwordSignup);
+        WebElement loin = driver.findElement(new By.ByXPath(LoginButton));
+        loin.click();
+        Thread.sleep(6000);
+
+    }
+
+    @Test
+    public void Retweet() throws InterruptedException {
+        WebElement retweet = driver.findElement(new By.ByXPath(secondRetweetImageNoNo));
+        retweet.click();
+        Thread.sleep(2000);
+        WebElement write = driver.findElement(By.xpath(imageTextEditLine)) ;
+        write.click();
+        Thread.sleep(2000);
+        write.sendKeys(retweetBody);
+        driver.findElement(MobileBy.AndroidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(100000)"));
+        Thread.sleep(2000);
+        WebElement post = driver.findElement(new By.ByXPath(postButton));
+        post.click();
+        Thread.sleep(4000);
+        if (driver.findElement(By.xpath(testTweet)).isDisplayed()){
+            System.out.println(pass);
+        }
+        else{
+            System.out.println(fail);
+        }
+
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (null != driver) {
+            driver.quit();
+        }
+    }
+}
---
+++ E2ETesting/src/test/java/Retweet/quoteTweetWithoutText.java
@@ -0,0 +1,76 @@
+package Retweet;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static variabales.selectors.*;
+
+public class quoteTweetWithoutText {
+    AppiumDriver driver;
+
+    @BeforeTest
+    public void setup() throws MalformedURLException, InterruptedException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automation", "UiAutomator2");
+        caps.setCapability("platformVersion", "9.0");
+        caps.setCapability("deviceName", "emulator-5554");
+        caps.setCapability("appPackage", "com.example.project_sw");
+        caps.setCapability("appActivity", ".MainActivity");
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+        /////////////////////////////////////////////////////////////////////////////////////////
+        Thread.sleep(4000);
+        WebElement loinButton = driver.findElement(new By.ByXPath(LoginButton));
+        loinButton.click();
+        Thread.sleep(2000);
+        WebElement usernameField = driver.findElement(new By.ByXPath(usernameLoginField));
+        usernameField.click();
+        Thread.sleep(2000);
+        usernameField.sendKeys(userSignup);
+        WebElement passField = driver.findElement(new By.ByXPath(passwordLoginField));
+        passField.click();
+        Thread.sleep(2000);
+        passField.sendKeys(passwordSignup);
+        WebElement loin = driver.findElement(new By.ByXPath(LoginButton));
+        loin.click();
+        Thread.sleep(6000);
+    }
+
+    @Test
+    public void Retweet() throws InterruptedException {
+        WebElement retweet = driver.findElement(new By.ByXPath(retweetText));
+        retweet.click();
+        Thread.sleep(2000);
+        WebElement post = driver.findElement(new By.ByXPath(postButton));
+        post.click();
+        Thread.sleep(2000);
+        if (driver.findElement(By.xpath(retweetedText)).isDisplayed()){
+            System.out.println(pass);
+        }
+        else{
+            System.out.println(fail);
+        }
+        if (driver.findElement(By.xpath(empty)).isDisplayed()){
+            System.out.println(pass);
+        }
+        else{
+            System.out.println(fail);
+        }
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (null != driver) {
+            driver.quit();
+        }
+    }
+}
