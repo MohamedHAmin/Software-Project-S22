aa3c7a43ea0c521b3fc72c56c4d48942b74fd366
Mary-ashraf <100151530+Mary-ashraf@users.noreply.github.com>
2022-05-14 11:19:42 +0200

    Comments edited

--- my-app/src/components/login/Signup.jsx
+++ my-app/src/components/login/Signup.jsx
@@ -69,10 +69,10 @@ const Signup = () => {
   });
 
   const validationSchema2 = Yup.object().shape({
-    phoneNumber: Yup.string().matches(
-      /^((\\+[1-9]{1,4}[ \\-])|(\\([0-9]{2,3}\\)[ \\-])|([0-9]{2,4})[ \\-])?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
-      "Phone number is not valid"
-    ),
+    // phoneNumber: Yup.string().matches(
+    //   /^((\\+[1-9]{1,4}[ \\-])|(\\([0-9]{2,3}\\)[ \\-])|([0-9]{2,4})[ \\-])?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
+    //   "Phone number is not valid"
+    // ),
     Biography: Yup.string(),
     // birth: { date: Yup.string().matches(/^(\S+$)/, 'you entered a space ,please remove it') }
   });
@@ -102,7 +102,7 @@ const Signup = () => {
       .post("http://larry-env.eba-u6mbx2gb.us-east-1.elasticbeanstalk.com/api/user/signup", data)
     console.log("🚀 ~ file: Signup.jsx ~ line 100 ~ onSubmit2 ~ signupRes", signupRes)
 
-      const userID = signupRes.data._id;
+      const userID = signupRes.data._id.toString();
       console.log("🚀 ~ file: Signup.jsx ~ line 96 ~ .then ~ userID", userID);
       
       if(banner){
