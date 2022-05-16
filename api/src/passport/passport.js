1afd5f5399504d74c7703792e150186472b9882c
Abdelkhalek7 <100151191+Abdelkhalek7@users.noreply.github.com>
2022-05-16 20:55:31 +0200

    add dashboard

--- api/src/passport/passport.js
+++ api/src/passport/passport.js
@@ -1,16 +1,35 @@
 const  User = require('../models/User')
+const  Token = require('../models/Token')
 const mongoose = require('mongoose')
 require('env-cmd')
+const generator = require('generate-password')
+const nodemailer = require("nodemailer")
+let transporter = nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+      user: process.env.AUTH_EMAIL,
+      pass: process.env.AUTH_PASS,
+    }
+  })
+const jwt=require('jsonwebtoken');
 const {v4: uuidv4 } = require("uuid")
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 //Please be noted that no duplicate emails are allowed 
+
+
 module.exports = function (passport){
-   
+    var newPassword = generator.generate({
+        length: 10,
+        numbers: true
+    });
+
     passport.use(new GoogleStrategy({
     clientID : process.env.GOOGLE_CLIENT_ID ,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL : '/user/auth/google/callback',
 }, async(accessToken, refreshToken , profile , done)=>{
+
+
     const newUser = { 
         googleId : profile.id,
         screenName: profile.displayName,
@@ -18,16 +37,42 @@ module.exports = function (passport){
         profileAvater: profile.photos[0].value,
         verified:true,
         email:profile.emails[0].value,
-        password:"googledummy123"
+        password:newPassword
     }
+
  try {
         let user = await User.findOne({ googleId: profile.id })
      if (user){
+        const token=jwt.sign({_id:user._id.toString()},process.env.SECRET,{
+
+            expiresIn: '24h' // expires in 365 days
+      
+       })
+         let newtoken = {
+             token : token,
+             ownerId: user._id,
+             expiredAt: Date.now() + 86400000
+         }
+         newtoken = await Token.create(newtoken)
+
         done(null,user)
         }
      else{
-         // creates a new user if not found 
         user = await User.create(newUser)
+        SetPassword(user.email,newPassword)
+        const token=jwt.sign({_id:user._id.toString()},process.env.SECRET,{
+
+            expiresIn: '24h' // expires in 365 days
+      
+       })
+         // creates a new user if not found and gives him a unique password
+        let newtoken = {
+            token : token,
+            ownerId: user._id,
+            expiredAt: Date.now() + 86400000
+        }
+        newtoken = await Token.create(newtoken)
+        
         //user.generateAuthToken();
         done(null,user)
         }
@@ -45,4 +90,27 @@ passport.serializeUser((user,done)=>{
 passport.deserializeUser((id,done)=>{
     User.findById(id, (err,user)=> done(err,user))
 })
+
 }
+
+const SetPassword = async (email,newPassword)=>{
+   
+    //delete any existing forgot password requests by the user 
+    try{
+
+  
+
+  const mailOptions = {
+    from : process.env.AUTH_EMAIL,
+    to: email,
+    subject : "Arriving from Google ? ",
+    text: `Your generarted password is : ${newPassword}`
+  }
+  
+      await transporter.sendMail(mailOptions)
+    }
+     catch(e)
+    {
+      console.log(e);
+    }
+  }
