a87d9ea190ff3585017e84a5662bc2d44339a306
Farah Gamal <73616568+FarahGamal@users.noreply.github.com>
2022-05-24 23:27:56 +0200

    search

---
+++ E2ETesting/src/test/java/search/searchFollowing.java
@@ -0,0 +1,74 @@
+package search;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static variabales.selectors.*;
+import static variabales.selectors.searchField;
+
+public class searchFollowing {
+    AppiumDriver driver;
+
+    @BeforeTest
+    public void setup() throws MalformedURLException, InterruptedException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automation", "UiAutomator2");
+        caps.setCapability("platformVersion", "9.0");
+        caps.setCapability("deviceName", "emulator-5554");
+        caps.setCapability("appPackage", "com.example.cross_platform_software_project");
+        caps.setCapability("appActivity", ".MainActivity");
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        Thread.sleep(8000);
+        WebElement loinButton = driver.findElement(new By.ByXPath(LoginButton));
+        loinButton.click();
+        Thread.sleep(5000);
+        WebElement usernameField = driver.findElement(new By.ByXPath(usernameLoginField));
+        usernameField.click();
+        Thread.sleep(2000);
+        usernameField.sendKeys(userSignup);
+        WebElement passField = driver.findElement(new By.ByXPath(passwordLoginField));
+        passField.click();
+        Thread.sleep(2000);
+        passField.sendKeys(passwordSignup);
+        WebElement loin = driver.findElement(new By.ByXPath(LoginButton));
+        loin.click();
+        Thread.sleep(8000);
+    }
+
+    @Test
+    public void notification() throws InterruptedException {
+        WebElement search = driver.findElement(new By.ByXPath(searchOption));
+        search.click();
+        Thread.sleep(2000);
+        WebElement field = driver.findElement(new By.ByXPath(searchField));
+        field.click();
+        Thread.sleep(2000);
+        field.sendKeys("admintag1");
+        Thread.sleep(3000);
+        TouchAction touchAction=new TouchAction(driver);
+        touchAction.tap(PointOption.point(114, 354)).perform();
+        Thread.sleep(4000);
+        WebElement find = driver.findElement(new By.ByXPath(profileoption));
+
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (null != driver) {
+            driver.quit();
+        }
+    }
+}
